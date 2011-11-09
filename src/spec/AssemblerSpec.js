6081d2207302ab098bb3183135057d3c83ecf576
DavidLudwig <dludwig@pobox.com>
2011-11-09 17:52:45 -0500

    early Assembler implementation: includes support for Exec and NoOp instructions, along with support for tags

---
+++ src/spec/AssemblerSpec.js
@@ -0,0 +1,95 @@
+
+describe("Assembler", function () {
+	theAssembler = null;
+	
+	beforeEach(function () {
+		this.addMatchers({
+			toBeInstructionOfType: function (expected) {
+				return (this.actual != null) && (this.actual[0] == expected);
+			}
+		});
+		
+		theAssembler = new Assembler();
+	});
+	
+	it("can assemble an empty program", function () {
+		var code = "";
+		var theProgram = theAssembler.assemble(code);
+		expect(theProgram).toBeDefined();
+		expect(theProgram.countInstructions()).toEqual(0);
+	});
+	
+	it("can assemble a single NoOp instruction", function () {
+		var code = "noop";
+		var theProgram = theAssembler.assemble(code);
+		expect(theProgram.countInstructions()).toEqual(1);
+		expect(theProgram.getInstruction(0)).toBeInstructionOfType(Instruction_NoOp);
+	});
+	
+	it("can assemble a multiple NoOp instructions, while ignoring empty lines and preceding whitespace", function () {
+		var code = "noop \n\
+					noop \n\
+					\n\
+					noop \n\
+					     \n\
+					noop \n\
+					noop";
+
+		var theProgram = theAssembler.assemble(code);
+		expect(theProgram.countInstructions()).toEqual(5);
+		for (var i = 0; i < 5; i++) {
+			expect(theProgram.getInstruction(i)).toBeInstructionOfType(Instruction_NoOp);
+		}
+	});
+	
+	it("can assemble Exec instructions", function () {
+		var code = "exec 1 + 2; \n\
+					exec 3 + 4; \n\
+					exec 52 * 3;";
+		var theProgram = theAssembler.assemble(code);
+		expect(theProgram.countInstructions()).toEqual(3);
+		expect(theProgram.getInstruction(0)).toBeInstructionOfType(Instruction_Exec);
+		expect(theProgram.getInstruction(1)).toBeInstructionOfType(Instruction_Exec);
+		expect(theProgram.getInstruction(2)).toBeInstructionOfType(Instruction_Exec);
+	});
+	
+	it("can read in tags", function () {
+		var code = ":Start \n\
+						noop \n\
+						noop \n\
+					:Position With Spaces \n\
+						noop \n\
+					:Final Position";
+		var theProgram = theAssembler.assemble(code);
+		expect(theProgram.getTag("Start")).toEqual(0);
+		expect(theProgram.getTag("Position With Spaces")).toEqual(2);
+		expect(theProgram.getTag("Final Position")).toEqual(3);
+	});
+		
+	describe("With Code Execution", function () {
+		theMachine = null;
+		
+		beforeEach(function () {
+			theMachine = new Machine();
+		})
+		
+		it("can assemble and run an empty program", function () {
+			var code = "";
+			var theProgram = theAssembler.assemble(code);
+			theMachine.loadProgram(theProgram);
+			expect(theMachine.getInstructionPointer()).toEqual(0);
+		});
+		
+		it("can run programs with multiple exec functions, with data shared among them", function () {
+			var code = "exec this.foo = this.foo * 2; \n\
+						exec this.foo += 2;";
+			var theProgram = theAssembler.assemble(code);
+			theMachine.loadProgram(theProgram);
+			var execEnv = new Array();
+			execEnv.foo = 10;
+			theMachine.setExecEnvironment(execEnv);
+			theMachine.run();
+			expect(execEnv.foo).toEqual(22);
+		});
+	});
+});
---
+++ src/src/Assembler.js
@@ -0,0 +1,57 @@
+
+function Assembler() {
+	// this.foo = 123;
+}
+
+// Program Management
+
+Assembler.prototype.assemble = function (code) {
+	var theProgram = new Program();
+	
+	var codeLines = code.split("\n");
+	//console.log("code lines: " + codeLines.length);
+	
+	for (var lineNumber = 1; lineNumber <= codeLines.length; lineNumber++) {
+		var codeLine = codeLines[lineNumber - 1];
+		//console.log("raw code: " + codeLine);
+
+		// strip preceding whitespace
+		codeLine = codeLine.replace(/^\s*/, "");
+		
+		// strip trailing whitespace
+		codeLine = codeLine.replace(/\s*$/, "");
+
+		// do nothing if there's no data in the line
+		if (codeLine.length == 0) {
+			continue;
+		}
+		
+		// check for a tag
+		if (codeLine[0] == ":") {
+			var tagName = codeLine.replace(/^\:/, "");
+			//console.log("tag: '" + tagName + "'");
+			theProgram.setTag(tagName);
+			continue;
+		}
+		
+		// retrieve instruction name
+		var instruction = codeLine.replace(/^([a-z]+).*/, "$1");
+		//console.log("instruction: " + instruction);
+		
+		// interpret the line
+		switch (instruction) {
+			case "noop":
+				theProgram.addInstruction(Instruction_NoOp);
+				break;
+				
+			case "exec":
+				var execCode = codeLine.replace(/^[a-z]+/, "");
+				//console.log("execCode @ " + lineNumber + ": '" + execCode + "'");
+				var theFunction = new Function(execCode);
+				theProgram.addInstruction(Instruction_Exec, theFunction);
+				break;
+		}
+	}
+	
+	return theProgram;
+}
